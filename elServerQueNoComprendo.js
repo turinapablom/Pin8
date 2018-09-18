11cd70ab4a90d07df7dee57b25b62bc3a38ba083
PabloTurina <turinapablom@gmail.com>
2018-09-18 12:30:13 -0300

    estoy listo para mudarme

---
+++ elServerQueNoComprendo.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 5000 // esto es un truquito para que heroku le asigne el puerto que quiera, y si no le asigna ninguno que use el puerto 5000
+
+app.get('/', (req, res) => res.send('que mirás!'))
+
+app.listen(PORT, () => console.log('nuestro server está escuchando en el puerto 5000!'))

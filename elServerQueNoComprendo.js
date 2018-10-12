02eac9317f1c3af4fc33f094801b1369d700a1b7
alejandro-panza <alejandrofpanza@gmail.com>
2018-10-12 10:26:36 -0300

    arreglado el post

--- elServerQueNoComprendo.js
+++ elServerQueNoComprendo.js
@@ -2,27 +2,10 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 5000 // esto es un truquito para que heroku le asigne el puerto que quiera, y si no le asigna ninguno que use el puerto 5000
 var bodyParser = require("body-parser");
-//Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(express.json())
-app.use(express.urlencoded())
-
-let posic1 = {
-    latitud: 13.232323,
-    longitud : 13.232323
-}
-let posic2 = {
-    latitud: 23.232323,
-    longitud : 23.232323
-}
-let posic3 = {
-    latitud: 33.232323,
-    longitud : 33.232323
-}
+app.use(bodyParser.urlencoded({ extended: false }))
 
 let posiciones = [posic1, posic2, posic3]
-
 
 app.get('/', (req, res) => res.sendfile('./html/gps.html'))
 
@@ -30,7 +13,7 @@ app.get('/telescopio', (req, res) => res.sendfile('./html/tokio.html'))
 
 app.post('/calculador', (request, response) => {
     console.log(request.body)
-    return response.send(posiciones)
+    return response.send([request.body])
 
 })
 

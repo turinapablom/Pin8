2a06885fea632f5a15febd1629e8e13ec9aaae07
pablolucianop <pablolucianop@gmail.com>
2018-10-16 20:55:53 -0300

    heroku

--- elServerQueNoComprendo.js
+++ elServerQueNoComprendo.js
@@ -5,16 +5,30 @@ var bodyParser = require("body-parser");
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
+var posiciones = {}
+
 //let posiciones = [posic1, posic2, posic3]
 
 app.get('/', (req, res) => res.sendfile('./html/gps.html'))
 
 app.get('/telescopio', (req, res) => res.sendfile('./html/tokio.html'))
 
-app.post('/calculador', (request, response) => {
-    console.log(request.body)
-    return response.send([request.body])
+// var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
 
+
+app.post('/calculador', (request, response) => {
+    //console.log(request.body)
+    //console.log(request)
+    var ip = request.connection.remoteAddress
+    posiciones[ip] = request.body
+    
+    console.log(posiciones)
+    return response.send(Object.values())
+ 
 })
 
+
+
 app.listen(PORT, () => console.log('nuestro server está escuchando en el puerto 5000!'))
+
+

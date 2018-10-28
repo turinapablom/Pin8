a4f591acbec62054d6b4ffb6e9aedc86e0f9a8bb
PabloTurina <turinapablom@gmail.com>
2018-10-28 20:24:33 -0300

    addUser incompleto PMT

---
+++ html/adduserPMT.js
@@ -0,0 +1,33 @@
+let usuarios = {
+    1: { // 1 es el id de Ale
+        puntos: 0,
+        persona: {
+            ultimasCoord: {
+                lat: 123,
+                long: 321,
+                accuracy: 12,
+                timestamp: '123443534'
+            },
+            nombre: 'Alejandro',
+            cadenaReferentes: [1]
+        },
+        mascota: {
+            nombre: 'esta',
+        },
+    }
+   
+};
+
+usuarios[2] = {
+    mascota: "loca"
+};
+
+console.log(usuarios);
+
+
+
+
+
+// let usuarios = ["a","e","y"]
+// usuarios.push('2')
+
